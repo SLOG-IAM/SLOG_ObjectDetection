6ff2604a201e0a90da0709340a6f44c21917b92e
SLOG-IAM <iam.ai.project.pl@gmail.com>
2019-12-10 18:39:05 +0900

    배포

---
+++ build/precache-manifest.3ad3791db9dd822cc1b138a71df8c4e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25087443c2e729149d63",
+    "url": "/SLOG_ObjectDetection/static/css/main.87e1a919.chunk.css"
+  },
+  {
+    "revision": "25087443c2e729149d63",
+    "url": "/SLOG_ObjectDetection/static/js/main.25087443.chunk.js"
+  },
+  {
+    "revision": "e6e3f32981dbf10cba69",
+    "url": "/SLOG_ObjectDetection/static/js/1.e6e3f329.chunk.js"
+  },
+  {
+    "revision": "c26e7059aa1bd7bedea6",
+    "url": "/SLOG_ObjectDetection/static/js/runtime~main.c26e7059.js"
+  },
+  {
+    "revision": "6f1d0728c19da67ae797269841020a9f",
+    "url": "/SLOG_ObjectDetection/index.html"
+  }
+];
